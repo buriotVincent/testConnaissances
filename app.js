def5844e4dd768d84d50a9c04cb464b1f69dba7a
buriotVincent <buriot.vincent@gmail.com>
2019-06-05 10:51:44 +0200

    test fonctionnement

---
+++ app.js
@@ -0,0 +1,36 @@
+let express = require('express');
+let session = require('cookie-session');
+let bodyParser = require('body-parser');
+
+let urlencodedParser = bodyParser.urlencoded({extended: false});
+
+let app = express();
+
+app.use(session({secret: "supersecret"}));
+
+app.use(function(req, res, next) {
+    if (typeof(req.session.array) === 'undefined') {
+        req.session.array = [];
+    }
+    next();
+});
+
+app.get('/index', function(req, res) {
+    res.render('index.ejs', {array: req.session.array});
+});
+
+app.post('/index/add', urlencodedParser, function(req, res) {
+    if (req.body.newelement !== '') {
+        req.session.array.push(req.body.newelement);
+    }
+    res.resirect('/index');
+});
+
+app.get('/index/delete/:index', function(req, res) {
+    if (req.params.index !== '') {
+        req.session.array.splice(req.params.index, 1);
+    }
+    res.redirect('/index');
+})
+
+app.listen(3389);
